25f9a1a2dd553b3a7aefe22d5a37cb42a93a84b7
marco-contreras <marco.contreras@gmail.com>
2018-05-03 16:49:24 -0700

    Directives Introduction

---
+++ vue.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '.vue',
+    data: {
+        show_text: true,
+        message: 'Message',
+        message_input_show: 'Do Enter',
+        message_input: '',
+        src: 'https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmReM2aoZysGbehUrN6RWkRfRLjaVuR3xL3XFpiY17x7QB',
+    }
+});
